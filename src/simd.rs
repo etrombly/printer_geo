65ae7e82ad63ca7bd4c9a09601d494d60cb78c44
Eric Trombly <etrombly@yahoo.com>
2020-08-24 12:40:39 -0500

    add some benches to test simd

---
+++ src/simd.rs
@@ -0,0 +1,288 @@
+use crate::geo::*;
+use rayon::iter;
+use rayon::prelude::*;
+use std::arch::x86_64::*;
+
+pub fn tri_bbox_simd(tris: &Vec<Triangle3d>) -> Vec<Line3d> {
+    let mut results: Vec<Line3d> = Vec::new();
+    let tri_chunks = tris.chunks_exact(8);
+    let remainder = tri_chunks.remainder();
+    for tri8 in tri_chunks {
+        unsafe {
+            let x1 = _mm256_set_ps(
+                tri8[0].p1.x,
+                tri8[1].p1.x,
+                tri8[2].p1.x,
+                tri8[3].p1.x,
+                tri8[4].p1.x,
+                tri8[5].p1.x,
+                tri8[6].p1.x,
+                tri8[7].p1.x,
+            );
+            let x2 = _mm256_set_ps(
+                tri8[0].p2.x,
+                tri8[1].p2.x,
+                tri8[2].p2.x,
+                tri8[3].p2.x,
+                tri8[4].p2.x,
+                tri8[5].p2.x,
+                tri8[6].p2.x,
+                tri8[7].p2.x,
+            );
+            let x3 = _mm256_set_ps(
+                tri8[0].p3.x,
+                tri8[1].p3.x,
+                tri8[2].p3.x,
+                tri8[3].p3.x,
+                tri8[4].p3.x,
+                tri8[5].p3.x,
+                tri8[6].p3.x,
+                tri8[7].p3.x,
+            );
+            let y1 = _mm256_set_ps(
+                tri8[0].p1.y,
+                tri8[1].p1.y,
+                tri8[2].p1.y,
+                tri8[3].p1.y,
+                tri8[4].p1.y,
+                tri8[5].p1.y,
+                tri8[6].p1.y,
+                tri8[7].p1.y,
+            );
+            let y2 = _mm256_set_ps(
+                tri8[0].p2.y,
+                tri8[1].p2.y,
+                tri8[2].p2.y,
+                tri8[3].p2.y,
+                tri8[4].p2.y,
+                tri8[5].p2.y,
+                tri8[6].p2.y,
+                tri8[7].p2.y,
+            );
+            let y3 = _mm256_set_ps(
+                tri8[0].p3.y,
+                tri8[1].p3.y,
+                tri8[2].p3.y,
+                tri8[3].p3.y,
+                tri8[4].p3.y,
+                tri8[5].p3.y,
+                tri8[6].p3.y,
+                tri8[7].p3.y,
+            );
+            let z1 = _mm256_set_ps(
+                tri8[0].p1.z,
+                tri8[1].p1.z,
+                tri8[2].p1.z,
+                tri8[3].p1.z,
+                tri8[4].p1.z,
+                tri8[5].p1.z,
+                tri8[6].p1.z,
+                tri8[7].p1.z,
+            );
+            let z2 = _mm256_set_ps(
+                tri8[0].p2.z,
+                tri8[1].p2.z,
+                tri8[2].p2.z,
+                tri8[3].p2.z,
+                tri8[4].p2.z,
+                tri8[5].p2.z,
+                tri8[6].p2.z,
+                tri8[7].p2.z,
+            );
+            let z3 = _mm256_set_ps(
+                tri8[0].p3.z,
+                tri8[1].p3.z,
+                tri8[2].p3.z,
+                tri8[3].p3.z,
+                tri8[4].p3.z,
+                tri8[5].p3.z,
+                tri8[6].p3.z,
+                tri8[7].p3.z,
+            );
+
+            let x_min = _mm256_min_ps(_mm256_min_ps(x1, x2), x3);
+            let y_min = _mm256_min_ps(_mm256_min_ps(y1, y2), y3);
+            let z_min = _mm256_min_ps(_mm256_min_ps(z1, z2), z3);
+            let x_max = _mm256_max_ps(_mm256_max_ps(x1, x2), x3);
+            let y_max = _mm256_max_ps(_mm256_max_ps(y1, y2), y3);
+            let z_max = _mm256_max_ps(_mm256_max_ps(z1, z2), z3);
+            let mut x_min_dst = [0f32; 8];
+            let mut y_min_dst = [0f32; 8];
+            let mut z_min_dst = [0f32; 8];
+            let mut x_max_dst = [0f32; 8];
+            let mut y_max_dst = [0f32; 8];
+            let mut z_max_dst = [0f32; 8];
+            _mm256_store_ps(x_min_dst.as_mut_ptr() as *mut _, x_min);
+            _mm256_store_ps(y_min_dst.as_mut_ptr() as *mut _, y_min);
+            _mm256_store_ps(z_min_dst.as_mut_ptr() as *mut _, z_min);
+            _mm256_store_ps(x_max_dst.as_mut_ptr() as *mut _, x_max);
+            _mm256_store_ps(y_max_dst.as_mut_ptr() as *mut _, y_max);
+            _mm256_store_ps(z_max_dst.as_mut_ptr() as *mut _, z_max);
+            for index in 0..8 {
+                results.push(Line3d {
+                    p1: Point3d::new(x_min_dst[index], y_min_dst[index], z_min_dst[index]),
+                    p2: Point3d::new(x_max_dst[index], y_max_dst[index], z_max_dst[index]),
+                });
+            }
+        }
+        for tri in remainder {
+            results.push(tri.bbox());
+        }
+    }
+    results
+}
+
+pub fn tri_bbox_simd_par(tris: &Vec<Triangle3d>) -> Vec<Line3d> {
+    let tri_chunks = tris.par_chunks_exact(8);
+    let remainder = tri_chunks.remainder();
+    let mut results: Vec<Line3d> = tri_chunks
+        .flat_map(|tri8| unsafe {
+            let x1 = _mm256_set_ps(
+                tri8[0].p1.x,
+                tri8[1].p1.x,
+                tri8[2].p1.x,
+                tri8[3].p1.x,
+                tri8[4].p1.x,
+                tri8[5].p1.x,
+                tri8[6].p1.x,
+                tri8[7].p1.x,
+            );
+            let x2 = _mm256_set_ps(
+                tri8[0].p2.x,
+                tri8[1].p2.x,
+                tri8[2].p2.x,
+                tri8[3].p2.x,
+                tri8[4].p2.x,
+                tri8[5].p2.x,
+                tri8[6].p2.x,
+                tri8[7].p2.x,
+            );
+            let x3 = _mm256_set_ps(
+                tri8[0].p3.x,
+                tri8[1].p3.x,
+                tri8[2].p3.x,
+                tri8[3].p3.x,
+                tri8[4].p3.x,
+                tri8[5].p3.x,
+                tri8[6].p3.x,
+                tri8[7].p3.x,
+            );
+            let y1 = _mm256_set_ps(
+                tri8[0].p1.y,
+                tri8[1].p1.y,
+                tri8[2].p1.y,
+                tri8[3].p1.y,
+                tri8[4].p1.y,
+                tri8[5].p1.y,
+                tri8[6].p1.y,
+                tri8[7].p1.y,
+            );
+            let y2 = _mm256_set_ps(
+                tri8[0].p2.y,
+                tri8[1].p2.y,
+                tri8[2].p2.y,
+                tri8[3].p2.y,
+                tri8[4].p2.y,
+                tri8[5].p2.y,
+                tri8[6].p2.y,
+                tri8[7].p2.y,
+            );
+            let y3 = _mm256_set_ps(
+                tri8[0].p3.y,
+                tri8[1].p3.y,
+                tri8[2].p3.y,
+                tri8[3].p3.y,
+                tri8[4].p3.y,
+                tri8[5].p3.y,
+                tri8[6].p3.y,
+                tri8[7].p3.y,
+            );
+            let z1 = _mm256_set_ps(
+                tri8[0].p1.z,
+                tri8[1].p1.z,
+                tri8[2].p1.z,
+                tri8[3].p1.z,
+                tri8[4].p1.z,
+                tri8[5].p1.z,
+                tri8[6].p1.z,
+                tri8[7].p1.z,
+            );
+            let z2 = _mm256_set_ps(
+                tri8[0].p2.z,
+                tri8[1].p2.z,
+                tri8[2].p2.z,
+                tri8[3].p2.z,
+                tri8[4].p2.z,
+                tri8[5].p2.z,
+                tri8[6].p2.z,
+                tri8[7].p2.z,
+            );
+            let z3 = _mm256_set_ps(
+                tri8[0].p3.z,
+                tri8[1].p3.z,
+                tri8[2].p3.z,
+                tri8[3].p3.z,
+                tri8[4].p3.z,
+                tri8[5].p3.z,
+                tri8[6].p3.z,
+                tri8[7].p3.z,
+            );
+
+            let x_min = _mm256_min_ps(_mm256_min_ps(x1, x2), x3);
+            let y_min = _mm256_min_ps(_mm256_min_ps(y1, y2), y3);
+            let z_min = _mm256_min_ps(_mm256_min_ps(z1, z2), z3);
+            let x_max = _mm256_max_ps(_mm256_max_ps(x1, x2), x3);
+            let y_max = _mm256_max_ps(_mm256_max_ps(y1, y2), y3);
+            let z_max = _mm256_max_ps(_mm256_max_ps(z1, z2), z3);
+            let mut x_min_dst = [0f32; 8];
+            let mut y_min_dst = [0f32; 8];
+            let mut z_min_dst = [0f32; 8];
+            let mut x_max_dst = [0f32; 8];
+            let mut y_max_dst = [0f32; 8];
+            let mut z_max_dst = [0f32; 8];
+            _mm256_store_ps(x_min_dst.as_mut_ptr() as *mut _, x_min);
+            _mm256_store_ps(y_min_dst.as_mut_ptr() as *mut _, y_min);
+            _mm256_store_ps(z_min_dst.as_mut_ptr() as *mut _, z_min);
+            _mm256_store_ps(x_max_dst.as_mut_ptr() as *mut _, x_max);
+            _mm256_store_ps(y_max_dst.as_mut_ptr() as *mut _, y_max);
+            _mm256_store_ps(z_max_dst.as_mut_ptr() as *mut _, z_max);
+            iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[0], y_min_dst[0], z_min_dst[0]),
+                p2: Point3d::new(x_max_dst[0], y_max_dst[0], z_max_dst[0]),
+            })
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[1], y_min_dst[1], z_min_dst[1]),
+                p2: Point3d::new(x_max_dst[1], y_max_dst[1], z_max_dst[1]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[2], y_min_dst[2], z_min_dst[2]),
+                p2: Point3d::new(x_max_dst[2], y_max_dst[2], z_max_dst[2]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[3], y_min_dst[3], z_min_dst[3]),
+                p2: Point3d::new(x_max_dst[3], y_max_dst[3], z_max_dst[3]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[4], y_min_dst[4], z_min_dst[4]),
+                p2: Point3d::new(x_max_dst[4], y_max_dst[4], z_max_dst[4]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[5], y_min_dst[5], z_min_dst[5]),
+                p2: Point3d::new(x_max_dst[5], y_max_dst[5], z_max_dst[5]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[6], y_min_dst[6], z_min_dst[6]),
+                p2: Point3d::new(x_max_dst[6], y_max_dst[6], z_max_dst[6]),
+            }))
+            .chain(iter::once(Line3d {
+                p1: Point3d::new(x_min_dst[7], y_min_dst[7], z_min_dst[7]),
+                p2: Point3d::new(x_max_dst[7], y_max_dst[7], z_max_dst[7]),
+            }))
+        })
+        .collect();
+    for tri in remainder {
+        results.push(tri.bbox());
+    }
+
+    results
+}
---
+++ src/util.rs
@@ -0,0 +1,142 @@
+use crate::geo::Triangle3d;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::{Error, ErrorKind, Result};
+
+pub struct Triangle {
+    pub normal: [f32; 3],
+    pub v1: [f32; 3],
+    pub v2: [f32; 3],
+    pub v3: [f32; 3],
+    pub attr_byte_count: u16,
+}
+
+fn point_eq(lhs: [f32; 3], rhs: [f32; 3]) -> bool {
+    lhs[0] == rhs[0] && lhs[1] == rhs[1] && lhs[2] == rhs[2]
+}
+
+impl PartialEq for Triangle {
+    fn eq(&self, rhs: &Triangle) -> bool {
+        point_eq(self.normal, rhs.normal)
+            && point_eq(self.v1, rhs.v1)
+            && point_eq(self.v2, rhs.v2)
+            && point_eq(self.v3, rhs.v3)
+            && self.attr_byte_count == rhs.attr_byte_count
+    }
+}
+
+impl Eq for Triangle {}
+
+pub struct BinaryStlHeader {
+    pub header: [u8; 80],
+    pub num_triangles: u32,
+}
+
+pub struct BinaryStlFile {
+    pub header: BinaryStlHeader,
+    pub triangles: Vec<Triangle>,
+}
+
+fn read_point<T: ReadBytesExt>(input: &mut T) -> Result<[f32; 3]> {
+    let x1 = input.read_f32::<LittleEndian>()?;
+    let x2 = input.read_f32::<LittleEndian>()?;
+    let x3 = input.read_f32::<LittleEndian>()?;
+
+    Ok([x1, x2, x3])
+}
+
+fn read_triangle<T: ReadBytesExt>(input: &mut T) -> Result<Triangle> {
+    let normal = read_point(input)?;
+    let v1 = read_point(input)?;
+    let v2 = read_point(input)?;
+    let v3 = read_point(input)?;
+    let attr_count = input.read_u16::<LittleEndian>()?;
+
+    Ok(Triangle {
+        normal,
+        v1,
+        v2,
+        v3,
+        attr_byte_count: attr_count,
+    })
+}
+
+fn read_header<T: ReadBytesExt>(input: &mut T) -> Result<BinaryStlHeader> {
+    let mut header = [0u8; 80];
+
+    match input.read(&mut header) {
+        Ok(n) => {
+            if n == header.len() {
+                ()
+            } else {
+                return Err(Error::new(ErrorKind::Other, "Couldn't read STL header"));
+            }
+        }
+        Err(e) => return Err(e),
+    };
+
+    let num_triangles = input.read_u32::<LittleEndian>()?;
+
+    Ok(BinaryStlHeader {
+        header,
+        num_triangles,
+    })
+}
+
+pub fn read_stl<T: ReadBytesExt>(input: &mut T) -> Result<BinaryStlFile> {
+    // read the header
+    let header = read_header(input)?;
+
+    let mut triangles = Vec::new();
+    for _ in 0..header.num_triangles {
+        triangles.push(read_triangle(input)?);
+    }
+
+    Ok(BinaryStlFile { header, triangles })
+}
+
+fn write_point<T: WriteBytesExt>(out: &mut T, p: [f32; 3]) -> Result<()> {
+    for x in &p {
+        out.write_f32::<LittleEndian>(*x)?;
+    }
+    Ok(())
+}
+
+pub fn write_stl<T: WriteBytesExt>(out: &mut T, stl: &BinaryStlFile) -> Result<()> {
+    assert_eq!(stl.header.num_triangles as usize, stl.triangles.len());
+
+    //write the header.
+    out.write_all(&stl.header.header)?;
+    out.write_u32::<LittleEndian>(stl.header.num_triangles)?;
+
+    // write all the triangles
+    for t in &stl.triangles {
+        write_point(out, t.normal)?;
+        write_point(out, t.v1)?;
+        write_point(out, t.v2)?;
+        write_point(out, t.v3)?;
+        out.write_u16::<LittleEndian>(t.attr_byte_count)?;
+    }
+
+    Ok(())
+}
+
+pub fn load_stl(file: &str) -> BinaryStlFile {
+    let file = File::open(file).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    read_stl(&mut buf_reader).unwrap()
+}
+
+pub fn to_triangles3d(file: &BinaryStlFile) -> Vec<Triangle3d> {
+    file.triangles
+        .iter()
+        .map(|x| {
+            Triangle3d::new(
+                (x.v1[0], x.v1[1], x.v1[2]),
+                (x.v2[0], x.v2[1], x.v2[2]),
+                (x.v3[0], x.v3[1], x.v3[2]),
+            )
+        })
+        .collect()
+}
